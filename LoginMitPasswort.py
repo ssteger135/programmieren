07c1d33be7184afd20d38be3309d4a09a92a4ff3
ssteger135 <96242095+ssteger135@users.noreply.github.com>
2021-12-16 14:14:39 +0100

    Add files via upload

--- LoginMitPasswort.py
+++ LoginMitPasswort.py
@@ -1,9 +1,10 @@
 # Hier gibt der Benutzer sein Paaswort ein
 x = input("Passwort eingeben: ")
 
-# Wenn das Passwort richtig ist, steht herzlich willkommen
+# Wenn das Passwort richtig ist, steht herzlich willkommen.
 if (int(x)) == 11:
     print("Herzlich willkommen")
 
+# Ist das Passwort falsch, steht Passwort nicht korrekt.
 else:
     print("Passwort nicht korrekt!")
